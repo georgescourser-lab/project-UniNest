c8c14c186fe175f8072a5655338058aae203c8a6
George Ochieng <georgescourser@gmail.com>
2026-09-06 02:42:27 +0300

    deployment

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     serverActions: {
       bodySizeLimit: '50mb',
     },
-    middlewareClientMaxBodySize: '50mb',
+    proxyClientMaxBodySize: '50mb',
   },
 };
 
